764f5f0090c451ca6c2890a51a3bb78f64667ac0
Dorari <76504846+Dorari@users.noreply.github.com>
2021-02-17 12:31:12 +0700

    Lap1

---
+++ test/src/Appheader.js
@@ -0,0 +1,12 @@
+function Appheader(){
+
+
+return(
+    <header className="App-header" >
+        <img className ="header-logo" src ="/pic/21720213.png"/>
+
+
+      </header>
+    );
+    }
+    export default Appheader;
---
+++ test/src/items.js
@@ -0,0 +1,27 @@
+const items =[
+    {
+      title :"รูป1",
+      thumbnaiUrl :"/pic/21720211.png"
+      
+    
+    },
+    {
+      title :"รูป2",
+      thumbnaiUrl :"/pic/21720212.png"
+      
+    
+    },
+    {
+      title :"รูป3",
+      thumbnaiUrl :"/pic/21720213.png"
+      
+    
+    },
+    {
+      title :"รูป4",
+      thumbnaiUrl :"/pic/21720214.jpg"
+      
+    
+    }
+  ];
+  export default items;
